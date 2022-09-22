9ff19a5ed4e71820d85210a4c505ace7df0d8c48
IgorEM <iem@cin.ufpe.br>
2022-09-22 20:54:13 -0300

    Outro componente de Descriçao

---
+++ src/examples/Cards/ProjectCards/Descricao1/Descricao1.jsx
@@ -0,0 +1,181 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v4.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Tooltip from "@mui/material/Tooltip";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftButton from "components/SoftButton";
+import SoftAvatar from "components/SoftAvatar";
+
+function DefaultProjectCard({ image, label, title, description, action, authors,endereco,tipo,comprimento }) {
+  const renderAuthors = authors.map(({ image: media, name }) => (
+    <Tooltip key={name} title={name} placement="bottom">
+      <SoftAvatar
+        src={media}
+        alt={name}
+        size="xs"
+        sx={({ borders: { borderWidth }, palette: { white } }) => ({
+          border: `${borderWidth[2]} solid ${white.main}`,
+          cursor: "pointer",
+          position: "relative",
+          ml: -1.25,
+
+          "&:hover, &:focus": {
+            zIndex: "10",
+          },
+        })}
+      />
+    </Tooltip>
+  ));
+
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "transparent",
+        boxShadow: "none",
+        overflow: "visible",
+      }}
+    >
+      <SoftBox position="relative" width="100.25%" shadow="xl" borderRadius="xl">
+        <CardMedia
+          src={image}
+          component="img"
+          title={title}
+          sx={{
+            maxWidth: "100%",
+            margin: 0,
+            boxShadow: ({ boxShadows: { md } }) => md,
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+      </SoftBox>
+      <SoftBox pt={3} px={0.5}>
+        <SoftBox mb={1}>
+          <SoftTypography
+            variant="button"
+            fontWeight="regular"
+            textTransform="capitalize"
+            textGradient
+          >
+            {label}
+          </SoftTypography>
+        </SoftBox>
+        <SoftBox mb={1}>
+          {action.type === "internal" ? (
+            <SoftTypography
+              component={Link}
+              to={action.route}
+              variant="h5"
+              textTransform="capitalize"
+            >
+              {title}
+            </SoftTypography>
+          ) : (
+            <SoftTypography
+              component="a"
+              href={action.route}
+              target="_blank"
+              rel="noreferrer"
+              variant="h5"
+              textTransform="capitalize"
+            >
+              {title}
+            </SoftTypography>
+          )}
+        </SoftBox>
+        <SoftBox mb={3} lineHeight={0}>
+          <SoftTypography variant="button" fontWeight="regular" color="text">
+            {description}
+          </SoftTypography>
+        </SoftBox>
+        <SoftBox mb={3} lineHeight={0}>
+          <SoftTypography variant="button" fontWeight="regular" color="text">  
+                    <div className="caixaFora"> 
+                   <div className="caixa">
+                      <div className="descricao">
+                      <div className='3infos'>            
+                        <h2 className='endereço'>{endereco}</h2>
+                        <h2 className="tipoMadeira">{tipo}</h2>
+                        <h2 className='comprimentoMadeira'>{comprimento}</h2>
+                      </div>
+                      <div className="perguntas">
+                        <h3 >Qual a quantidade disponível? </h3>
+                        <p> 30 sacos de 3kg </p>
+                        <h3 >Quais os dias disponiveis? </h3>
+                        <p>
+                        Domingo: - <br />
+                        Segunda-feira: 09:00 - 18:00   19:00 - 20:00<br />
+                        Terça-feira: 09:00 - 18:00<br />
+                        Quarta-feira: 09:00 - 18:00<br />
+                        Quinta-feira: 09:00 - 18:00<br />
+                        Sexta-feira: 09:00 - 18:00<br />
+                        Sábado: 09:00 - 12:00<br />
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+        </SoftTypography>
+        </SoftBox>
+      </SoftBox>
+    </Card>
+  );
+}
+
+// Setting default values for the props of DefaultProjectCard
+DefaultProjectCard.defaultProps = {
+  authors: [],
+};
+
+// Typechecking props for the DefaultProjectCard
+DefaultProjectCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  action: PropTypes.shape({
+    type: PropTypes.oneOf(["external", "internal"]),
+    route: PropTypes.string.isRequired,
+    color: PropTypes.oneOf([
+      "primary",
+      "secondary",
+      "info",
+      "success",
+      "warning",
+      "error",
+      "light",
+      "dark",
+      "white",
+    ]).isRequired,
+    label: PropTypes.string.isRequired,
+  }).isRequired,
+  authors: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default DefaultProjectCard;
