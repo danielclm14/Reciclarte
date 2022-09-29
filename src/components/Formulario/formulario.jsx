1bf8374bebbbe358ed9bb42ea58ab3d71f4a01a0
IgorEM <iem@cin.ufpe.br>
2022-09-28 21:45:31 -0300

    componente Formulario e inserindo ele na rota inserirItem

---
+++ src/components/Formulario/formulario.jsx
@@ -0,0 +1,74 @@
+import React, { Component }  from 'react';
+
+export default class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('Item Registrado com sucesso: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Tamanho  
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+            Quantidade  
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+          Tipo de Madeira  
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="pinus">Pinus</option>
+            <option value="carvalho">Carvalho</option>
+            <option value="ipê">Ipê</option>
+            <option value="peroba">Peroba</option>
+          </select>
+        </label>
+        <label>
+            Domingo
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+            <h3>Disponibilidade:</h3>
+            <label>
+            Segunda 
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Terça
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Quarta 
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Quinta
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Sexta
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Sábado
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label><br/>
+          <input type="submit" value="Enviar" />
+        </form>
+      );
+    }
+}
--- src/layouts/inserirItem/index.js
+++ src/layouts/inserirItem/index.js
@@ -18,68 +18,32 @@ import Card from "@mui/material/Card";
 
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
-import SoftTypography from "components/SoftTypography";
+
 
 // Soft UI Dashboard React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
+
 
 
 // Data
 import Grid from "@mui/material/Grid";
-import pinus3 from "../../imagens/pinus3.jpg";
-//import DefaultProjectCard from "../../examples/Cards/ProjectCards/DefaultProjectCard"
-import Descricao1 from "../../examples/Cards/ProjectCards/Descricao1/Descricao1";
-import Descricao from "../../examples/Cards/StatisticsCards/Descricao/Descricao";
-import PopUp from "../../components/PopUp/PopUp";
-import Botao from "../../components/SoftButton/SoftButtonRoot";
-import SoftButton from "../../components/SoftButton";
-
+//-------
+import Formulario from "../../components/Formulario/formulario"
 
 function Inserir() {
   
-
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <SoftBox py={3}>
-      
-        
-          {/* <SoftBox display="flex" justifyContent="space-between" alignItems="center" px={1}>
-            <SoftTypography variant="h6">Descrição do Item</SoftTypography>
-          </SoftBox> */}
                 <Grid container spacing={3}>
                   <Grid item xs={12} md={2} xl={3}>
-                      {/* <Descricao1
-                              image={pinus3}
-                              // label="project #2"
-                              title="Madereira X"
-                              action={{
-                                type: "internal",
-                                route: "/tables",
-                              }}
-                              description="A madereira X é top"
-                              
-                              endereco="Endereço"
-                              tipo="Pinus"
-                              comprimento="2x3x4"
-                          
-                            /> */}
-                            
-        
-                  </Grid>
-                  <Grid item xs={12} md={2} xl={3}>
-                  {/* <PopUp /> */}
-
+                  <Formulario />
                   </Grid>
                 </Grid>
-                
-        
       </SoftBox>
-      
     </DashboardLayout>
   );
 }
-
 export default Inserir;
