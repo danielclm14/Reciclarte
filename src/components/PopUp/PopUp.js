aba60869aa7f0c2469fe475f93f361c132728203
erbert-gadelha <ebgr@cin.ufpe.br>
2022-10-01 16:26:31 -0300

    codigo de reseva no front

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,7 +1,4 @@
 import React, { Component }  from 'react';
-import PopUpCodigo from "../PopUpCodigo/PopUpCodigo"
-import BotaoCodigo from "../BotaoCodigo/BotaoCodigo"
-
 
 export default class PopUp extends React.Component {
   state = {
@@ -9,6 +6,7 @@ export default class PopUp extends React.Component {
     value: 0,
     warn1: '.',
     warn2: '.',
+    sel_popup: true,
   }
 
   constructor() {
@@ -67,22 +65,18 @@ export default class PopUp extends React.Component {
   }
 
   submeter() {
-    let retorno = {data:this.data, quantidade:this.value,};
     
-    if(this.validar())
-    this.props.fechar(retorno);
+    if(this.validar()) {
+      // COMUNICA COM O BACK
+      // ABRE O CODIGO DE RESERVA
+      this.setState({sel_popup:false});
+    }
      
   }
 
   fechar() {
     let retorno = {data: this.data, quantidade: -1,}
     this.props.fechar(retorno);
-  }
-
-  abrirpopup(){
-    return( 
-    < PopUpCodigo />
-    );
   }
 
   increment() {
@@ -100,6 +94,194 @@ export default class PopUp extends React.Component {
     if (this.value <= 0) return;
     this.setState({ value: this.value - 1 });
   }
+
+  //  SELECIONA DATA E QUANTIDADE
+  pop1() {
+
+    // STYLES
+      var styles_box = {
+      position:'relative',
+      display: 'flex',
+      'align-items': 'center',
+      'justify-content': 'flex-start',
+      'flex-direction': 'column',
+      'font-weight': '500',
+      'font-size': '15px',
+      color: 'black',
+      
+      width: '320px',
+      'border-radius': '7px',
+      'background-color': 'rgba(248, 249, 250, 1)',
+      padding: '30px',
+      }; var styles_input = {
+      'background-color': 'white',
+      border: '2px black solid',
+      'border-radius': '5px',
+      'font-size': '15px',  
+      width: '150px',
+      height: '25px',
+      padding: '0px',
+      }; var styles_btn = {
+        'background-color': 'rgba(76, 175, 80, 1)',
+        color: 'white',
+        border: 'none',
+        'margin-top': '20px',
+      
+        'font-size': '15px',
+        'font-weight': '500',
+        width: '100%',
+        height: '50px',
+        
+      }; var style_fechar =  {
+        'background-color': 'brown',
+        color: 'white',
+        border: 'none',
+        position: 'relative',
+      
+        top: '-20px',
+        right: '-132px',
+      
+        'font-size': '15px',
+        'font-weight': '500',
+        
+        width: '25px',
+        height: '25px',
+      
+        padding: "0px",
+        'border-radius': '5px',
+      }; var input_number = {
+      'background-color': 'white',
+      border: '2px black solid',
+      'border-radius': '5px',
+      color: 'black',
+
+      display: 'flex',
+      'flex-direction': 'row',
+      'justify-content': 'center',
+      'align-items': 'center',
+      gap: '18px',
+
+      width: '80px',
+      }; var input_number_button = {
+      'background-color': 'transparent',
+      'font-weight': '500',
+      'font-size': '20px',
+      color: 'black',
+      border: 'none',
+      padding: '0px',
+      margin: '0px',
+      height: '100%',
+      width: '50px',
+
+      'transition': '200ms all ease-in-out',
+      '-webkit-transition': '200ms all ease-in-out',
+      '-moz-transition': '200ms all ease-in-out',
+      '-ms-transition': '200ms all ease-in-out',
+      '-o-transition': '200ms all ease-in-out',
+      }
+    //
+
+    return <div style={styles_box}>
+    <div style={{position: 'absolute'}}> <button onClick={this.fechar} style={style_fechar}>⨉</button> </div>
+      Escolha a data
+      <input
+          type="date"
+          onChange={evt => this.atualizar_data(evt)}
+          style={styles_input}
+      />
+      <span style={{color:"red", fontSize:"10px"}}>
+        {this.state.warn1}
+      </span>
+
+      Quantidade de sacos
+      
+      <div style={input_number}>
+        <button style={input_number_button} onClick={this.decrement}>&minus;</button>
+        <span>{this.value}</span>
+        <button style={input_number_button} onClick={this.increment}>&#43;</button>     
+      </div>
+      <span style={{color:"red", fontSize:"10px"}}>
+        {this.state.warn2}
+      </span>
+      {/*<br/>*/}
+      <button style={styles_btn} onClick={this.submeter}>Vrau</button>
+      {/*<BotaoCodigo />*/}
+  </div>;
+  }
+
+  //  EXIBE CODIGO DE RESERVA
+  pop2() {
+
+    // STYLES
+      var styles_box = {
+        position:'relative',
+        display: 'flex',
+        'align-items': 'center',
+        'justify-content': 'flex-start',
+        'flex-direction': 'column',
+        'font-weight': '500',
+        'font-size': '15px',
+        color: 'black',
+        
+        width: '320px',
+        'border-radius': '7px',
+        'background-color': 'rgba(248, 249, 250, 1)',
+        padding: '30px',
+      }; var styles_btn = {
+          'background-color': 'rgba(76, 175, 80, 1)',
+          color: 'white',
+          border: 'none',
+          'margin-top': '20px',
+        
+          'font-size': '15px',
+          'font-weight': '500',
+          width: '100%',
+          height: '50px',
+          
+      }; var style_fechar =  {
+          'background-color': 'brown',
+          color: 'white',
+          border: 'none',
+          position: 'relative',
+        
+          top: '-20px',
+          right: '-132px',
+        
+          'font-size': '15px',
+          'font-weight': '500',
+          
+          width: '25px',
+          height: '25px',
+        
+          padding: "0px",
+          'border-radius': '5px',
+      };
+    //
+  
+    return (
+        <div style={styles_box}>
+          <div style={{position: 'absolute'}}> <button onClick={this.fechar} style={style_fechar}>⨉</button> </div>
+            Código de Reserva
+            <input 
+                type="text"
+                value="465654"
+                style={{textAlign:"center"}}
+             />
+  
+            <span style={{color:"red", fontSize:"10px"}}>
+              {this.state.warn1}
+            </span>
+  
+            <p>Informe seu código na retirada do produto</p>
+            
+            <span style={{color:"red", fontSize:"10px"}}>
+              {this.state.warn2}
+            </span>
+            {/*<br/>*/}
+            <button style={styles_btn} onClick={this.fechar}>OK</button>
+        </div>
+      );
+    }
   
   render() {
 
@@ -199,33 +381,8 @@ export default class PopUp extends React.Component {
 
     return (      
       <div style={styles_bkg}>
-      <div style={styles_box}>
-        <div style={{position: 'absolute'}}> <button onClick={this.fechar} style={style_fechar}>⨉</button> </div>
-          Escolha a data
-          <input
-              type="date"
-              onChange={evt => this.atualizar_data(evt)}
-              style={styles_input}
-          />
-          <span style={{color:"red", fontSize:"10px"}}>
-            {this.state.warn1}
-          </span>
-
-          Quantidade de sacos
-          
-          <div style={input_number}>
-            <button style={input_number_button} onClick={this.decrement}>&minus;</button>
-            <span>{this.value}</span>
-            <button style={input_number_button} onClick={this.increment}>&#43;</button>     
-          </div>
-          <span style={{color:"red", fontSize:"10px"}}>
-            {this.state.warn2}
-          </span>
-          {/*<br/>*/}
-          {/* <button style={styles_btn} onClick={this.submeter}>Vrau</button> */}
-          <BotaoCodigo />
+          {(this.state.sel_popup?this.pop1():this.pop2())}
       </div>
-    </div>
     );
   }
 }
